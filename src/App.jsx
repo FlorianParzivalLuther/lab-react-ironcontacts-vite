e3dc99b49319e4340d42ecb64fd52ef94a7a61ea
Florian Luther <hi@gauvainstudios.com>
2023-07-11 16:16:17 +0200

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,51 @@ import contacts from "./contacts.json";
 function App() {
   const initialContacts = contacts.slice(0, 5);
   const [contactList, setContactList] = useState(initialContacts);
+
   const newArray = contacts.filter(function (el) {
     return !initialContacts.includes(el);
   });
   const randomContact = newArray[Math.floor(Math.random() * newArray.length)];
 
-
-  function handleSort(){
-    const sortedData=[...contactList].sort((a,b)=>{
-      return a.name>b.name ? 1 : -1
-    })
+  function handleSort() {
+    const sortedData = [...contactList].sort((a, b) => {
+      return a.name > b.name ? 1 : -1;
+    });
     setContactList(sortedData);
-    console.log(contactList)
   }
 
-  
-
-
-
+  const sortByPopularity = () => {
+    const sortedContacts = [...contactList].sort((a, b) => {
+      return b.popularity - a.popularity;
+    });
+    setContactList(sortedContacts);
+  };
 
   return (
     <div>
       <h1>ACTORS</h1>
       <table>
         <thead>
-        <tr>
-          <th><button
-        onClick={() => {
-          setContactList((prevContacts) => [...prevContacts, randomContact]);
-        }}
-      >
-        Add New Actor
-      </button></th>
-      <th><button onClick={handleSort}>Sort by Name</button></th>
-      <th><button>Sort by Popularity</button></th>
-
-        </tr>
+          <tr>
+            <th>
+              <button
+                onClick={() => {
+                  setContactList((prevContacts) => [
+                    ...prevContacts,
+                    randomContact,
+                  ]);
+                }}
+              >
+                Add New Actor
+              </button>
+            </th>
+            <th>
+              <button onClick={handleSort}>Sort by Name</button>
+            </th>
+            <th>
+              <button onClick={sortByPopularity}>Sort by Popularity</button>
+            </th>
+          </tr>
           <tr>
             <th>Picture</th>
             <th>Name</th>
@@ -63,8 +72,6 @@ function App() {
           ))}
         </tbody>
       </table>
-
-      
     </div>
   );
 }
